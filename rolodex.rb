d7b7fd9129d340a05e8a2c8691194aab2efc1b94
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-01-27 15:40:38 -0500

    Added rolodex.rb and filled with code from last week. Made crm.rb require rolodex

---
+++ rolodex.rb
@@ -0,0 +1,13 @@
+class Rolodex
+	attr_reader :contacts 
+	@@ids = 001
+	def initialize
+		@contacts = []
+		# @ids = 001
+	end
+
+	def add_contact(contact)
+		contact.id = @@ids
+		@contacts << contact
+		@@ids += 1
+	end
