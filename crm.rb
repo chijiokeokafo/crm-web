c3bdced2dfe99fd0a6c40c062cf617b7552afd2d
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-01-27 15:37:27 -0500

    added code to contact.rb

--- crm.rb
+++ crm.rb
@@ -7,8 +7,12 @@ get '/' do
   erb :index
 end
 
-get '/contacts' do
-  @crm_app_name = "My CRM"		
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
   erb :contacts
 end
 
