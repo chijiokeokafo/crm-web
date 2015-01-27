a953dd29036cfbc4f052167fb1ab8bc9b1693161
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-01-27 15:46:15 -0500

    Removed @contacts array and replaced with an instance of the  class

--- crm.rb
+++ crm.rb
@@ -2,7 +2,7 @@ require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
 
-# $rolodex = Rolodex.new
+$rolodex = Rolodex.new
 
 get '/' do
   @crm_app_name = "My CRM"		
@@ -10,11 +10,6 @@ get '/' do
 end
 
 get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
   erb :contacts
 end
 
